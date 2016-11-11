020fe42d0338f485a367d8c66499c2bdd68c5811
(cdep)illabout <cdep.illabout@gmail.com>
2016-11-11 13:29:13 +0900

    Add a simple skeleton file.

---
+++ src/Mail/Hailgun/Simple.hs
@@ -0,0 +1,15 @@
+{- |
+Module      :  Mail.Hailgun.Simple
+Copyright   :  Dennis Gosnell 2016
+License     :  BSD3
+Maintainer  :  Dennis Gosnell (cdep.illabout@gmail.com)
+Stability   :  experimental
+Portability :  unknown
+
+module description
+-}
+
+module Mail.Hailgun.Simple
+  (
+  ) where
+
